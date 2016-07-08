b99cd1984cd8d92d8fd1016f3fb6f3c3aa325b6d
曲云浩 <quyunhaoai@163.com>
2016-07-08 15:22:08 +0800

    Initial Commit

---
+++ ApplePay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePay
+//
+//  Created by hao on 16/7/8.
+//  Copyright © 2016年 hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ApplePay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ApplePay
+//
+//  Created by hao on 16/7/8.
+//  Copyright © 2016年 hao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
